375d3b3b851fbc84461de0b14f3a80c2e3ec34d1
Ben Rathke <ben.rathke@gmail.com>
2021-09-08 15:13:07 -0500

    11_Update

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -54,7 +54,7 @@ console.log("working");
 //}).addTo(map);
 
 //We create the street view tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -69,15 +69,15 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-  Street: streets,
-  Dark: dark
+  "Day Navigation": streets,
+  "Night Navigation": dark
 };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [30, 30],
+  center: [44, -80],
   zoom: 2,
-  layers: [streets]
+  layers: [dark]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
@@ -86,26 +86,44 @@ L.control.layers(baseMaps).addTo(map);
 //Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = "https://raw.githubusercontent.com/BenjaminRathke/Mapping_Earthquakes/main/torontoRoutes.json";
+
 // Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/BenjaminRathke/Mapping_Earthquakes/main/majorAirports.json";
+//let airportData = "https://raw.githubusercontent.com/BenjaminRathke/Mapping_Earthquakes/main/majorAirports.json";
+
+// Create a style for the lines.
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2
+}
 
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoData).then(function(data) {
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
-L.geoJson(data).addTo(map);
 L.geoJson(data, {
+  style: myStyle,
   onEachFeature: function(feature, layer) {
-    console.log(layer);
-    layer.bindPopup("<h2> Airport Code: " + feature.properties.faa + "</h2>" + 
-      "<hr> Airport name: " + feature.properties.name + "</hr>")
+    layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: " + feature.properties.dst + "</h3>");
   }
 }).addTo(map);
-
 
 });
 
 
+// Grabbing our GeoJSON data.
+//d3.json(airportData).then(function(data) {
+  //console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+//L.geoJson(data).addTo(map);
+//L.geoJson(data, {
+  //onEachFeature: function(feature, layer) {
+    //console.log(layer);
+    //layer.bindPopup("<h2> Airport Code: " + feature.properties.faa + "</h2>" + 
+     // "<hr> Airport name: " + feature.properties.name + "</hr>")
+  //}
+//}).addTo(map);
 
 //Get data from cities.js
 //let cityData = cities;
@@ -122,5 +140,3 @@ L.geoJson(data, {
   //.bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
 //.addTo(map);
 //});
-
-
